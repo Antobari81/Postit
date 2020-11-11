31a8069a7779696bb31e50d31802013b9e82e326
Antonella Barisone <32391606+Antobari81@users.noreply.github.com>
2020-11-11 23:47:42 +0000

    Create admin-header.php

---
+++ admin-header.php
@@ -0,0 +1,14 @@
+<!-- html code to show your post-it -->
+ <!-- add this to your file wp-admin/admin-header.php -->
+
+	<div id="post-it" class="side-spark">
+		<div class="close">		
+			<span onclick="myFunction()">CLOSE</span>  <!-- change span with button if you like -->
+		</div>
+			<div class="text-spark">
+				text for your post-it
+			</div>
+		</div>
+	</div>
+
+  <!-- end of admin-header.php customization -->
